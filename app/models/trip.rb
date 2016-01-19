4f7631d03eb19cd7f65e1cc8d60e4219e21fb73a
Meder <m.toktosunov@icloud.com>
2016-01-18 19:16:31 -0500

    added trip model tests

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,10 @@ class Trip < ActiveRecord::Base
   validates :origin, presence: true
   validates :destination, presence: true
   validates :meet_point, presence: true
+  validates :rate, presence: true
+  validates :car_make, presence: true
+  validates :car_model, presence: true
+  validates :car_plate, presence: true
+  validates :car_plate, presence: true
+  validates :avail_cap, presence: true
 end
